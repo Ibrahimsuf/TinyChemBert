7c16e671871a424d923d91b87478af6dbfe28121
Ibrahimsuf <110345830+Ibrahimsuf@users.noreply.github.com>
2024-06-25 19:10:59 -0500

    Train Test Splitting Code

--- ChemBert/utils.py
+++ ChemBert/utils.py
@@ -2,6 +2,7 @@ from datasets import load_dataset
 from deepchem.feat.smiles_tokenizer import SmilesTokenizer
 from torch.utils.data import DataLoader
 from typing import Optional
+import random
 allowed_atoms = {"C", "N", "O", "S", "P", "F", "Cl", "Br", "I"}
 def create_simple_smiles(all_smiles_file: str, simple_smiles_file: str) -> None:
   """Create a list of SMILES string with less than 20 characters and only allowed atoms"""
@@ -20,6 +21,29 @@ def remove_unused_atoms_from_vocab_file(vocab_file: str, output_file: str) -> No
       atoms = set([char for char in line if char.isalpha()])
       if atoms.issubset(allowed_atoms):
         out.write(line)
+def split_train_test_val(examples_file: str, train_file: str, test_file: str, val_file: str, train_split: float, val_split: float) -> None:
+  """Split the train file into train, test, and val files"""
+  if train_split + val_split > 1:
+    raise ValueError("train_split + val_split must be less than 1")
+  
+  num_lines = sum(1 for line in open(examples_file, "r"))
+  num_train = int(num_lines * train_split)
+  num_val = int(num_lines * val_split)
+  permutation = random.sample(range(num_lines), num_lines)
+
+  train_indexes = permutation[:num_train]
+  val_indexes = permutation[num_train:num_train+num_val]
+  test_indexes = permutation[num_train+num_val:]
+  
+  with open(examples_file, "r") as f, open(train_file, "w") as train, open(test_file, "w") as test, open(val_file, "w") as val:
+    for i, line in enumerate(f):
+      if i in train_indexes:
+        train.write(line)
+      elif i in val_indexes:
+        val.write(line)
+      elif i in test_indexes:
+        test.write(line)
+
 class GetDataLoader:
   def __init__(self, file:str) -> None:
     self.file = file
