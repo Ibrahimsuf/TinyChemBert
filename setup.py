80cb43303d06b226d247f9b68097be8669f347c3
Ibrahimsuf <110345830+Ibrahimsuf@users.noreply.github.com>
2024-06-27 07:19:51 -0500

    Add dependencies

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
   name="ChemBert",
   version="0.1",
   packages=find_packages(),
-  install_requires=["transformers", "datasets", "torch"]
+  install_requires=["transformers", "datasets", "torch", "tqdm", "deepchem", "numpy==1.24"] #deepchem does not support numpy 2.0
 )
